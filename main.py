0e420d34f55b9dcaffb072a00e7539ed0e60e20f
Ashkrb <krenczb@gmail.com>
2024-03-19 18:35:29 +0100

    wrote a function to count the words in given string

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+def word_count(file_name):
+    new_words = []
+    count = 0
+    new_words = file_name.split()
+    for words in new_words:
+        count += 1
+    print(count)
+    return count
+    # counts the words in a file by splitting it and then iterating over the block with a counter
 def main():
+    
     with open("books/Frankenstein.txt") as f:
      file_contents = f.read()
      print(file_contents)
-
+     word_count(file_contents)
+     
 main()
