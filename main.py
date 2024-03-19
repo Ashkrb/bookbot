a79f0c4b552aeee079c11dce2086e3b38b783a61
Ashkrb <krenczb@gmail.com>
2024-03-19 19:56:24 +0100

    wrote funtionality to do two things: book_report sorts the previously made list of records by value, and then printed a neat report in the main function with the results we've had so far.

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ def word_appear(file_name):
           word_counter[word] += 1
         else: 
           word_counter[word] = 1
-    print(word_counter)
+   #print(word_counter)
     return word_counter
 #puts entire string into lowercase, then splits it, and populates the directory with character counts.
 
+def book_report(file_name):
+    char_dict = word_appear(file_name)
+    word_counter = word_count(file_name)
+    dict_list = []
+    for char in char_dict:
+       
+       dict_list.append({"char": char, "num":char_dict[char]})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
 
+def sort_on(dict):
+    return dict["num"]
 
 
 def word_count(file_name):
@@ -19,7 +30,7 @@ def word_count(file_name):
     new_words = file_name.split()
     for words in new_words:
         count += 1
-    print(count)
+    #print(count)
     return count
     # counts the words in a file by splitting it and then iterating over the block with a counter
 def main():
@@ -27,7 +38,18 @@ def main():
     with open("books/Frankenstein.txt") as f:
      file_contents = f.read()
      #print(file_contents)
-     word_count(file_contents)
-     word_appear(file_contents)
-     
+     #word_count(file_contents)
+     #word_appear(file_contents)
+     wordc = word_count(file_contents)
+     sorted_list = book_report(file_contents)
+     print("--- Begin report of books/frankenstein.txt ---")
+     print("f{wordc} words found in the document")
+     for item in sorted_list:
+        if not item["char"].isalpha():
+           continue
+        char = item["char"]
+        num = item["num"]
+        print(f" The {char} character was found {num} times")
+     print("--- End report ---")
+
 main()
